16aa8513e6da9f744aa6da927b3eba979da73ba3
Brendan Hurd <brendanhurd@gmail.com>
2023-08-04 18:51:00 +1000

    Create menu and areana pages. Create inital routes.

---
+++ src/Routes/Menu.tsx
@@ -0,0 +1,10 @@
+import { Link } from 'react-router-dom';
+
+export const Menu = () => {
+  return (
+    <div>
+      <Link to={'/arena'}>Arena</Link>
+      <h1>Menu Page</h1>
+    </div>
+  );
+};
--- src/main.tsx
+++ src/main.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App.tsx';
-import { createBrowserRouter, RouterProvider } from 'react-router-dom';
+// import App from './App.tsx';
+import {
+  createBrowserRouter,
+  createRoutesFromElements,
+  Route,
+  RouterProvider
+} from 'react-router-dom';
+import ErrorPage from './Routes/ErrorPage.tsx';
 import './index.css';
+import { Menu } from './Routes/Menu.tsx';
+import { Arena } from './Routes/Arena.tsx';
 
-const router = createBrowserRouter([
-  {
-    path: '/',
-    element: <App />
-  }
-]);
+const router = createBrowserRouter(
+  createRoutesFromElements(
+    <Route errorElement={<ErrorPage />}>
+      <Route path="/" element={<Menu />} />
+      <Route path="/arena" element={<Arena />} />
+    </Route>
+  )
+);
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
